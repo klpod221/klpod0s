65b2c3d6ea40466b643b8a6df65b755fe683a397
klpod221 <klpod221@gmail.com>
2023-10-24 17:06:28 +0700

    update installer

--- scripts/install_cfg.sh
+++ scripts/install_cfg.sh
@@ -66,17 +66,6 @@ fi
 
 ./scripts/create_cache.sh
 
-./scripts/install_zsh.sh
+# ./scripts/install_zsh.sh
 
-find "$CloneDir" -type l | while read -r slink; do
-    read_slink=$(readlink "$slink")
-    fixd_slink=$(echo "$read_slink" | awk -F '/.config/' '{print $NF}')
-    linkd_file=$(echo "$slink" | awk -F '/.config/' '{print $NF}')
-    echo "linking $HOME/.config/$linkd_file --> $HOME/.config/$fixd_slink..."
-    ln -fs $HOME/.config/$fixd_slink $HOME/.config/$linkd_file
-done
-
-if printenv HYPRLAND_INSTANCE_SIGNATURE &>/dev/null; then
-    echo "reloading hyprland..."
-    hyprctl reload
-fi
+./scripts/install_lnk.sh
