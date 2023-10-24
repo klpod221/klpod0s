9a643a0b986bfd2dbb351bc03de94a4b02d17f0d
klpod221 <klpod221@gmail.com>
2023-10-24 16:15:37 +0700

    update installer

--- scripts/install_lnk.sh
+++ scripts/install_lnk.sh
@@ -8,9 +8,9 @@ fi
 
 find $CloneDir -type l | while read slink
 do
-    read_slink=$(readlink $slink)
-    fixd_slink=$(echo $read_slink | awk -F '/.config/' '{print $NF}')
-    linkd_file=$(echo $slink | awk -F '/.config/' '{print $NF}')
+    read_slink=`readlink $slink`
+    fixd_slink=`echo $read_slink | awk -F '/.config/' '{print $NF}'`
+    linkd_file=`echo $slink | awk -F '/.config/' '{print $NF}'`
     echo "linking $HOME/.config/$linkd_file --> $HOME/.config/$fixd_slink..."
     ln -fs $HOME/.config/$fixd_slink $HOME/.config/$linkd_file
 done
